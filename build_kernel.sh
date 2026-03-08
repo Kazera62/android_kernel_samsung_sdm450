6bc57d91859b5cdb1036fbd1d72de593efc9f3c9
João Pedro <78806767+ifoodplayer@users.noreply.github.com>
2026-03-08 18:13:15 +0000

    vdso: Fix build issues

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,11 +4,12 @@ export ARCH=arm64
 mkdir out
 
 BUILD_CROSS_COMPILE=$(pwd)/toolchain/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
+BUILD_CROSS_COMPILE_ARM32=$(pwd)/toolchain/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship-10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a11q_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_ARM32=$BUILD_CROSS_COMPILE_ARM32 CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a11q_open_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_ARM32=$BUILD_CROSS_COMPILE_ARM32 CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
