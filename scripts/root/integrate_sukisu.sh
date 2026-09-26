b302d043d3e66254f1dc2bd275eacdd8f78f7c00
Kazera62 <kemanaapa207@gmail.com>
2026-09-26 17:07:44 +0700

    fix: repair malformed Linux 4.9 sepolicy wrapper transform

--- scripts/root/integrate_sukisu.sh
+++ scripts/root/integrate_sukisu.sh
@@ -1475,30 +1475,33 @@ struct selinux_policy *ksu_dup_sepolicy(struct selinux_policy *old_pol)
     void *data;
     struct policy_file fp;
 
-    if old_pol:
-        source = &old_pol->policydb
-        len = source->len
-        if not len:
-            return ERR_PTR(-EINVAL)
+    if (old_pol) {
+        source = &old_pol->policydb;
+        len = source->len;
+        if (!len)
+            return ERR_PTR(-EINVAL);
 
-        data = vmalloc(len)
-        if not data:
-            return ERR_PTR(-ENOMEM)
+        data = vmalloc(len);
+        if (!data)
+            return ERR_PTR(-ENOMEM);
 
-        fp.data = data
-        fp.len = len
-        ret = policydb_write(source, &fp)
-        if ret:
-            vfree(data)
-            return ERR_PTR(ret)
-    else:
-        ret = security_read_policy(&data, &len)
-        if ret:
-            return ERR_PTR(ret)
-        if not data or not len:
-            if data:
-                vfree(data)
-            return ERR_PTR(-EINVAL)
+        fp.data = data;
+        fp.len = len;
+        ret = policydb_write(source, &fp);
+        if (ret) {
+            vfree(data);
+            return ERR_PTR(ret);
+        }
+    } else {
+        ret = security_read_policy(&data, &len);
+        if (ret)
+            return ERR_PTR(ret);
+        if (!data || !len) {
+            if (data)
+                vfree(data);
+            return ERR_PTR(-EINVAL);
+        }
+    }
 
     new_pol = kzalloc(sizeof(*new_pol), GFP_KERNEL);
     if (!new_pol) {
